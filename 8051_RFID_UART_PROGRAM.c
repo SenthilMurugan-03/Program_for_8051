0aa2adb548710805ff862b21daab2fff263df403
Senthil_03 <115228001+SenthilMurugan-03@users.noreply.github.com>
2023-07-21 15:19:42 +0530

    Add files via upload

---
+++ 8051_RFID_UART_PROGRAM.c
@@ -0,0 +1,52 @@
+#include<AT89s8252.h>
+
+char msg[]="";
+char *p = msg;
+
+void serial_init()
+{
+	TR1 = 0;
+	TMOD = (TMOD & 0x0F) | 0x20;
+	TH1 = 0xFD;
+	TL1 = 0xFD;
+	SCON = 0x50;
+	PCON = (PCON & 0x7F) ;
+	TR1 = 1;
+}
+
+void read_serial()
+{
+	while(RI == 0);
+	RI = 0;
+	while(SBUF != '$')
+	{
+		*p = SBUF;
+		*p++;
+	}
+	*p = '\0';
+	p = msg;	
+}
+
+void print_serial(char *m)
+{
+	while(*m != '\0')
+	{
+		SBUF = *m;
+		*m++;
+		while(TI == 0);
+		TI = 0;
+	}	
+}
+
+void main()
+{
+	serial_init();
+	while(1)
+	{
+		read_serial();
+
+		print_serial("\nMessage Received : \n");
+		print_serial(msg);
+		
+	}
+}
